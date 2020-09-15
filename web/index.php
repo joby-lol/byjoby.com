a4521634ef7dac487d70fbc52149d34fb31d166d
Joby Elliott <joby@byjoby.com>
2020-09-15 15:16:20 -0600

    converting to leafcutter, moving content to separate repo

--- web/index.php
+++ web/index.php
@@ -1,12 +1,45 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . "/../core.php";
+namespace Leafcutter;
 
-//parse file
-$parser      = new Mni\FrontYAML\Parser();
-$document    = $parser->parse(file_get_contents(__DIR__ . '/index.md'));
-$frontMatter = $document->getYAML();
-$content     = $document->getContent();
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
-//send front matter into Template and echo content
-byjoby\Templates\TwigPage::set($frontMatter);
-echo $content;
+require __DIR__ . '/../vendor/autoload.php';
+
+date_default_timezone_set("America/Denver");
+
+//initialize configuration
+$config = new Config\Config();
+$config['base_dir'] = __DIR__;
+$config->readDir(__DIR__ . '/../config/', null, true);
+$config->readFile(__DIR__ . '/../config/env.yaml', null, true);
+
+//initialize logger
+$logger = new Logger('leafcutter');
+if ($config['debug_log']) {
+    $logger->pushHandler(
+        new StreamHandler(__DIR__ . '/../logs/debug.log', Logger::DEBUG)
+    );
+}
+$logger->pushHandler(
+    new StreamHandler(__DIR__ . '/../logs/error.log', Logger::ERROR)
+);
+
+//initialize URL site and context
+URLFactory::beginSite($config['site.url']);
+URLFactory::beginContext(); //calling without argument sets context to site
+
+//normalize URL
+URLFactory::normalizeCurrent();
+
+//initialize CMS context
+Leafcutter::beginContext($config, $logger);
+$leafcutter = Leafcutter::get();
+$leafcutter->content()->addDirectory($config['site.content_dir']);
+
+//build response from URL
+$response = $leafcutter->buildResponse(URLFactory::current());
+
+//render response
+$response->renderHeaders();
+$response->renderContent();
--- web/test/test.md
+++
@@ -1,6 +0,0 @@
----
-page_title: Test page
----
-# This is a test MD file
-
-It should get parsed and look **nice**
