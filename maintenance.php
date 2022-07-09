1daca88a51c46f936dd0e56c98c7e8865f50844d
Joby Elliott <code@byjoby.com>
2022-07-08 21:23:56 -0600

    fix for maintenance title tag

--- maintenance.php
+++ maintenance.php
@@ -7,9 +7,7 @@ http_response_code(503);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        <title>503 Service Unavailable</title>
-    </title>
+    <title>503 Service Unavailable</title>
 </head>
 
 <body>
