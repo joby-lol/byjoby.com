2e466e044e46c218ae27ac8520221e0b08235988
Joby Elliott <code@byjoby.com>
2022-07-09 10:23:35 -0600

    improvement to script

--- scripts/copy-production.php
+++ scripts/copy-production.php
@@ -5,11 +5,6 @@ use DigraphCMS\Config;
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../initialize.php';
 
-// only works on mysql
-if (Config::get('db.adapter') !== 'mysql') {
-    throw new \Exception("This script only works for MySQL databases");
-}
-
 // get username/password from config
 $user = Config::get('db.user');
 $pass = Config::get('db.pass');
@@ -30,6 +25,7 @@ $production_pdo = new PDO(
 );
 
 // drop all staging tables
+echo "Dropping staging tables";
 $staging_pdo->beginTransaction();
 $staging_pdo->exec('SET FOREIGN_KEY_CHECKS=0;');
 foreach ($staging_pdo->query('SHOW TABLES')->fetchAll() as $r) {
@@ -44,6 +40,7 @@ $staging_pdo->exec('SET FOREIGN_KEY_CHECKS=1;');
 $staging_pdo->commit();
 
 // copy all production tables into staging
+echo "Copying production tables to staging";
 $production_pdo->beginTransaction();
 $production_pdo->exec('SET FOREIGN_KEY_CHECKS=0;');
 foreach ($production_pdo->query('SHOW TABLES')->fetchAll() as $r) {
@@ -52,7 +49,7 @@ foreach ($production_pdo->query('SHOW TABLES')->fetchAll() as $r) {
         'CREATE TABLE `%s`.`%s` LIKE `%s`.`%s`',
         $staging_db,
         $r[0],
-        $production_pdo,
+        $production_db,
         $r[0]
     ));
     if ($query->execute()) {
@@ -65,7 +62,7 @@ foreach ($production_pdo->query('SHOW TABLES')->fetchAll() as $r) {
         'INSERT `%s`.`%s` SELECT * FROM `%s`.`%s`',
         $staging_db,
         $r[0],
-        $production_pdo,
+        $production_db,
         $r[0]
     ));
     if ($query->execute()) {
