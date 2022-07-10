4302769dbdc1e138868697cd756a5859184b3ab8
Joby Elliott <code@byjoby.com>
2022-07-10 15:32:39 -0600

    improvements for small screens

--- templates/sections/header.php
+++ templates/sections/header.php
@@ -7,8 +7,11 @@ use DigraphCMS\UI\UserMenu;
 ?>
 <header id="header">
     <div class="header__branding">
-        <span class="header__branding__machine"><?php echo Session::uuid(); ?>@jobys_site</span>:<span class="header__branding__path"><?php echo Context::url()->path(); ?></span><span style="white-space:nowrap;">$
-        <span class="header__branding__cursor">&#x2588;</span></span>
+        <span class="header__branding__machine"><?php echo Session::uuid(); ?>@jobys_site</span>:
+        <span class="header__branding__path"><?php echo Context::url()->path(); ?></span>
+        <span style="white-space:nowrap;">
+            $<span class="header__branding__cursor">&#x2588;</span>
+        </span>
     </div>
     <div class="header__usermenu">
         <?php echo new UserMenu; ?>
