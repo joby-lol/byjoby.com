5804ebd743d7245b88ca5aa95394f9bdb96a93b1
Joby Elliott <code@byjoby.com>
2022-07-10 18:10:26 -0600

    updated crt effect and header order

--- templates/sections/header.php
+++ templates/sections/header.php
@@ -1,14 +1,44 @@
 <?php
 
+use DigraphCMS\Cache\Cache;
 use DigraphCMS\Context;
-use DigraphCMS\Session\Session;
 use DigraphCMS\UI\UserMenu;
+use DigraphCMS\URL\URL;
+use DigraphCMS\Users\Users;
+
+$breadcrumb = Cache::get(
+    'header/breadcrumb' . md5(Context::url()->path()),
+    function () {
+        $breadcrumb = [];
+        $path = '';
+        foreach (explode('/', Context::url()->path()) as $part) {
+            if ($part) {
+                $path .= '/' . $part;
+                if (strpos($part, '.') === false) $url = new URL($path . '/');
+                else $url = new URL($path);
+                $part = sprintf('<a href="%s">%s</a>', $url, $part);
+            }
+            $breadcrumb[] = $part;
+        }
+        return implode('/', $breadcrumb);
+    }
+);
+
+if ($user = Users::current()) {
+    $user = sprintf(
+        '<a href="%s">%s</a>',
+        $user->profile(),
+        preg_replace('/[^a-z]+/', '_', strtolower($user->name()))
+    );
+} else {
+    $user = 'guest';
+}
 
 ?>
 <header id="header">
     <div class="header__branding">
-        <span class="header__branding__machine"><span style="font-weight:200;"><?php echo Session::uuid(); ?></span>@<span style="font-weight:800;">byjoby</span></span>:
-        <span class="header__branding__path"><?php echo Context::url()->path(); ?></span>
+        <span class="header__branding__machine"><span style="font-weight:200;"><?php echo $user; ?></span>@<a href="<?php echo new URL("/"); ?>" style="font-weight:800;">byjoby</a></span>:
+        <span class="header__branding__path"><?php echo $breadcrumb; ?></span>
         <span class="header__branding__prompt">
             $&nbsp;<span class="header__branding__cursor">&#x2588;</span>
         </span>
